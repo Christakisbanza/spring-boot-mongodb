4f47eb8026e8453314ace5bff7ec6a1177776f80
Christakis <banzachristakis@gmail.com>
2024-11-09 21:02:19 -0300

    add data in database

---
+++ src/main/java/com/mongoDB/aula_mongoDB/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.mongoDB.aula_mongoDB.config;
+
+import com.mongoDB.aula_mongoDB.domain.entities.User;
+import com.mongoDB.aula_mongoDB.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+    }
+
+
+}
